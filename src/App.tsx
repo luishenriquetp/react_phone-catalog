979d6eec89c2c60e079eea78571324eb6a287b41
luishenriquetp <luishenriquetp@gmail.com>
2024-06-06 15:28:03 -0300

    Footer removed
    Footer removed, added to its own branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import Header from './components/Header/Header';
 import Content from './components/Content/Content';
-import Footer from './components/Footer/Footer';
 
 const App: React.FC = () => (
 	<>
 		<Header />
 		<Content />
-		<Footer />
 	</>
 );
 
