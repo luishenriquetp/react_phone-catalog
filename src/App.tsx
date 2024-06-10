e7c5a2e20b2a886414926f47fe4a93c2b997f9b4
Rafael Wesley <rafaelwesleylima@gmail.com>
2024-06-10 16:16:26 -0300

    header and pagination merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import Header from './components/Header/Header';
 import Content from './components/Content/Content';
+import { Route, Routes } from 'react-router-dom';
+import PageCatalog from './pages/PageCatalog/PageCatalog';
 
 const App: React.FC = () => (
 	<>
 		<Header />
+		<div className='container' >
+			<Routes>
+				<Route path="/" />
+				<Route path="/phones" element={<PageCatalog />} />
+			</Routes>
+		</div>
 		<Content />
 	</>
 );
