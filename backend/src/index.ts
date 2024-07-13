a2ba262249218926e2dcd4ddf23aa5e01372a1d3
Rafael Wesley <rafaelwesleylima@gmail.com>
2024-07-13 15:53:51 -0300

    add pack, unfinished task

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,6 +7,4 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello, world!");
 });
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+app.listen(port, () => {});
